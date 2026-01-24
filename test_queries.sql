f15b394fae063ef9a317f9fe4afe2e3d46c65610
maya silver <mcsilver99@gmail.com>
2026-01-24 15:33:49 -0800

    Add test_queries.sql

---
+++ test_queries.sql
@@ -0,0 +1,24 @@
+-- Query 1: Select all AQI records
+SELECT *
+FROM AQI_Record;
+
+-- Query 2: Select AQI records with paramter name PM2.5
+SELECT *
+FROM AQI_Record
+WHERE parameter_name = 'PM2.5';
+
+-- Query 3: Sort all the AQI records in the Good category by site code
+SELECT *
+FROM AQI_Record
+WHERE category = 'Good'
+ORDER BY site_code;
+
+-- Query 4: Using DISTINCT to remove duplicates. Shows 
+SELECT DISTINCT parameter_name
+FROM AQI_Record;
+
+-- Query 5: Using LIMIT or OFFSET to control the number of results returned
+SELECT state_code, county_code, date, aqi
+FROM AQI_Record
+ORDER BY date DESC
+LIMIT 5;
